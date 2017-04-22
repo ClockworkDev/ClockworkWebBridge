d563c76ae40c253251ef9670d47e8444e0c2c5db
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2017-04-22 14:44:08 +0200

    Promises / build function  exported

--- index.js
+++ index.js
@@ -6,41 +6,51 @@ const contentFolderName = "/gamefiles";
 const templateFolderName = "template";
 
 function build(path) {
-    fs.removeSync(tempFolderName);
-    fs.mkdirSync(tempFolderName);
-    console.log("Decompressing files");
-    decompress(path, tempFolderName).then(files => {
-        console.log("Reading manifest");
-        var manifest = require("./" + tempFolderName + "/manifest.json");
-        fs.removeSync(manifest.name);
-        fs.mkdirSync(manifest.name);
-        fs.mkdirSync(manifest.name + contentFolderName);
-        console.log("Copying files");
-        fs.copySync(tempFolderName + "/" + manifest.scope, manifest.name + contentFolderName);
-        fs.copySync(templateFolderName, manifest.name);
-        var RTapis = fs.readFileSync(manifest.name + "/clockwork/RTpolyfill.js", "utf-8");
-        var updatedRTapis = RTapis.replace("/*manifest*/{}", JSON.stringify(manifest));
-        fs.writeFileSync(manifest.name + "/clockwork/RTpolyfill.js", updatedRTapis, "utf-8");
+    return new Promise(function (resolve, reject) {
         fs.removeSync(tempFolderName);
-        fs.mkdirSync(manifest.name + "/dependencies");
-        console.log("Downloading dependencies");
-        for (var name in manifest.dependencies) {
-            var version = manifest.dependencies[name];
-            downloadDependency(manifest.name, name, version);
-        }
+        fs.mkdirSync(tempFolderName);
+        console.log("Decompressing files");
+        decompress(path, tempFolderName).then(files => {
+            console.log("Reading manifest");
+            var manifest = require("./" + tempFolderName + "/manifest.json");
+            fs.removeSync(manifest.name);
+            fs.mkdirSync(manifest.name);
+            fs.mkdirSync(manifest.name + contentFolderName);
+            console.log("Copying files");
+            fs.copySync(tempFolderName + "/" + manifest.scope, manifest.name + contentFolderName);
+            fs.copySync(templateFolderName, manifest.name);
+            var RTapis = fs.readFileSync(manifest.name + "/clockwork/RTpolyfill.js", "utf-8");
+            var updatedRTapis = RTapis.replace("/*manifest*/{}", JSON.stringify(manifest));
+            fs.writeFileSync(manifest.name + "/clockwork/RTpolyfill.js", updatedRTapis, "utf-8");
+            fs.removeSync(tempFolderName);
+            fs.mkdirSync(manifest.name + "/dependencies");
+            console.log("Downloading dependencies");
+            var dependencyPromises = [];
+            for (var name in manifest.dependencies) {
+                var version = manifest.dependencies[name];
+                dependencyPromises.push(downloadDependency(manifest.name, name, version));
+            }
+            Promise.all(dependencyPromises).then(function () {
+                console.log("Project exported succesfully");
+                resolve(manifest.name);
+            });
+        });
     });
 }
 
 function downloadDependency(manifestName, name, version) {
-    request('http://cwpm.azurewebsites.net/api/packages/' + name + "/" + version, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            if (body == "") {
-                downloadDependency(manifestName, name, version);
-            } else {
-                fs.mkdirSync(manifestName + "/dependencies/" + name);
-                fs.writeFileSync(manifestName + "/dependencies/" + name + "/" + version, body, "utf-8");
+    return new Promise(function (resolve, reject) {
+        request('http://cwpm.azurewebsites.net/api/packages/' + name + "/" + version, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                if (body == "") {
+                    downloadDependency(manifestName, name, version).then(resolve);
+                } else {
+                    fs.mkdirSync(manifestName + "/dependencies/" + name);
+                    fs.writeFileSync(manifestName + "/dependencies/" + name + "/" + version, body, "utf-8");
+                    resolve();
+                }
             }
-        }
+        });
     });
 }
 
